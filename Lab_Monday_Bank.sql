72eb30164060a05a34c772cd66b06e2e5179462e
Mathilde Marie Monge <126065064+mathildemariemonge@users.noreply.github.com>
2023-04-03 15:04:31 +0200

    finished mandatory

--- Lab_Monday_Bank.sql
+++ Lab_Monday_Bank.sql
@@ -15,3 +15,42 @@ SELECT amount
 FROM bank.loan
 ORDER BY amount ASC
 LIMIT 3;
+
+SELECT DISTINCT status
+FROM bank.loan
+ORDER BY status;
+
+SELECT loan_id
+FROM bank.loan
+ORDER BY payments DESC
+LIMIT 1;
+
+
+SELECT account_id, amount
+FROM bank.loan
+ORDER BY account_id
+LIMIT 5;
+
+SELECT account_id
+FROM bank.loan
+WHERE duration IN (60)
+ORDER BY amount
+LIMIT 5;
+
+SELECT DISTINCT k_symbol
+FROM bank.`order`;
+
+SELECT DISTINCT account_id
+FROM bank.`order`
+WHERE order_id BETWEEN (29540) AND (29560);
+
+SELECT amount
+FROM bank.`order`
+WHERE account_to = (30067122);
+
+SELECT trans_id, date, type, amount
+FROM bank.trans
+WHERE account_id = (793)
+ORDER BY date DESC
+LIMIT 10;
+
