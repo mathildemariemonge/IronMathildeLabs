6ff9c917fa8234b474b03504555a1f4401be9c0e
Mathilde Marie Monge <126065064+mathildemariemonge@users.noreply.github.com>
2023-04-04 14:52:41 +0200

    added the first lab for tuesday

---
+++ SQL Lab Mandatory Tuesday.sql
@@ -0,0 +1,34 @@
+SELECT first_name, last_name, count(fa.film_id)
+FROM sakila.actor AS a
+JOIN sakila.film_actor AS fa
+ON a.actor_id=fa.actor_id
+GROUP BY a.actor_id
+ORDER BY count(fa.film_id) DESC
+LIMIT 1;
+
+SELECT first_name, last_name, count(r.rental_id)
+FROM sakila.customer AS c
+JOIN sakila.rental AS r
+ON c.customer_id=r.customer_id
+GROUP BY c.customer_id
+ORDER BY count(r.rental_id) DESC
+LIMIT 1;
+
+SELECT c.name, count(r.film_id)
+FROM sakila.category AS c
+JOIN sakila.film_category AS r
+ON c.category_id=r.category_id
+GROUP BY c.name
+ORDER BY c.name;
+
+SELECT s.first_name, s.last_name, a.address
+FROM sakila.staff AS s
+JOIN sakila.address AS a
+ON s.address_id=a.address_id
+GROUP BY s.staff_id;
+
+SELECT f.title AS title, l.name AS language
+FROM sakila.film AS f
+JOIN sakila.language AS l
+ON f.language_id=l.language_id
+WHERE f.title like 'M%';
