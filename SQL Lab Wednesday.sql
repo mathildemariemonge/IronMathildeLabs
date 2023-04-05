04c2ed83b1cdecc6f55c0cf9a55ea2faf49715de
Mathilde Marie Monge <126065064+mathildemariemonge@users.noreply.github.com>
2023-04-05 16:14:20 +0200

    add mandatory work

--- SQL Lab Wednesday.sql
+++ SQL Lab Wednesday.sql
@@ -1,79 +1,46 @@
-SELECT s.store_id, ci.city, co.country
-FROM sakila.store AS s
-JOIN sakila.address AS a
-ON s.address_id=a.address_id
-JOIN sakila.city AS ci
-ON a.city_id=ci.city_id
-JOIN sakila.country AS co
-ON ci.country_id=co.country_id;
+SELECT title
+FROM sakila.film
+WHERE length > (SELECT AVG(length)
+				FROM sakila.film);
+                
+SELECT COUNT(inventory_id)
+FROM sakila.inventory
+WHERE film_id = (SELECT film_id
+					FROM sakila.film
+                    WHERE title = 'Hunchback Impossible');
+                    
+                    
+SELECT first_name, last_name
+FROM sakila.actor
+WHERE actor_id IN (SELECT actor_id
+					FROM sakila.film_actor
+                    WHERE film_id IN (SELECT film_id
+										FROM sakila.film
+                                        WHERE title = 'Alone Trip'));
+                                        
+SELECT title
+FROM sakila.film
+WHERE film_id IN (SELECT film_id
+					FROM sakila.film_category
+                    WHERE category_id IN (SELECT category_id
+										FROM sakila.category
+                                        WHERE name = 'Family'));                             
 
-SELECT s.store_id, ci.city, co.country
-FROM sakila.store AS s
-JOIN sakila.address AS a
-ON s.address_id=a.address_id
-JOIN sakila.city AS ci
-ON a.city_id=ci.city_id
-JOIN sakila.country AS co
-ON ci.country_id=co.country_id;
+SELECT first_name, last_name, email
+FROM sakila.customer
+WHERE address_id IN (SELECT address_id
+						FROM sakila.address
+                        WHERE city_id IN (SELECT city_id
+											FROM sakila.city
+                                            WHERE country_id IN (SELECT country_id
+																	FROM sakila.country
+                                                                    WHERE country = 'Canada')));
+                                                                    
+SELECT cu.first_name, cu.last_name, cu.email
+FROM sakila.customer AS cu
+JOIN sakila.address AS a ON cu.address_id=a.address_id
+JOIN sakila.city AS ci ON a.city_id=ci.city_id
+JOIN sakila.country AS co ON ci.country_id=co.country_id
+WHERE co.country='Canada';
 
-SELECT s.store_id, sum(p.amount) 
-FROM sakila.staff AS s
-JOIN sakila.payment AS p
-ON s.staff_id=p.staff_id
-GROUP BY s.store_id;
-
-SELECT c.name, avg(f.length)
-FROM sakila.category AS c
-JOIN sakila.film_category AS r
-ON c.category_id=r.category_id
-JOIN sakila.film AS f
-ON f.film_id=r.film_id
-GROUP BY c.name
-ORDER BY c.name;
-
-SELECT c.name, avg(f.length)
-FROM sakila.category AS c
-JOIN sakila.film_category AS r
-ON c.category_id=r.category_id
-JOIN sakila.film AS f
-ON f.film_id=r.film_id
-GROUP BY c.name
-ORDER BY avg(f.length) DESC
-LIMIT 5;
-
-SELECT f.title
-FROM sakila.rental AS r
-JOIN sakila.inventory AS i
-ON r.inventory_id=i.inventory_id
-JOIN sakila.film AS f
-ON f.film_id=i.film_id
-GROUP BY f.film_id
-ORDER BY count(r.rental_id) DESC
-LIMIT 5;
-
-SELECT c.name AS category, sum(p.amount) AS revenue
-FROM sakila.payment AS p
-JOIN sakila.rental AS r
-ON p.rental_id=r.rental_id
-JOIN sakila.inventory AS i
-ON i.inventory_id=r.inventory_id
-JOIN sakila.film_category AS fc
-ON i.film_id=fc.film_id
-JOIN sakila.category AS c
-ON fc.category_id=c.category_id
-GROUP BY category
-ORDER BY sum(p.amount) DESC
-LIMIT 5;
-
-SELECT DISTINCT f.title,
-CASE 
-	WHEN r.return_date IS NULL THEN '0'
-    ELSE '1'
-END AS on_stock
-FROM sakila.rental AS r
-JOIN sakila.inventory AS i
-ON r.inventory_id=i.inventory_id
-JOIN sakila.film AS f
-ON f.film_id=i.film_id
-WHERE f.title IN ('Academy Dinosaur')
-LIMIT 1;
+SELECT 
